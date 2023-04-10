787193e86f3722ccdb2db1cc7a6264d847f0e41d
Your Name <daniel.paton.lr@gmail.com>
2023-04-10 16:24:52 +0200

    Change name

--- src/app/components/chatgpt/chatgpt.component.ts
+++ src/app/components/chatgpt/chatgpt.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// const { Configuration, OpenAIApi } = require('openai');
 import { Configuration, OpenAIApi } from 'openai';
 
 @Component({
@@ -8,111 +7,64 @@ import { Configuration, OpenAIApi } from 'openai';
   styleUrls: ['./chatgpt.component.scss'],
 })
 export class ChatgptComponent implements OnInit {
-  message: string = '';
-  chatHistory: { message: string; response: any }[] = [];
-  openAi: any;
-  version = '5';
-  isLoading: boolean = false;
-  costMsg;
+  message = '';
+  chatHistory = [];
+  openAi: OpenAIApi;
+  isLoading = false;
+  costMsg = '';
+  versionGPT = '3';
 
   constructor() {}
 
-  ngOnInit() {
-    this.openAi = this.createOpenAiClient(
-      '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'
-    );
+  ngOnInit(): void {
+    const apiKey = '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';
+    this.openAi = this.createOpenAiClient(apiKey);
   }
 
-  createOpenAiClient(apiKey: string) {
+  createOpenAiClient(apiKey: string): OpenAIApi {
     const configuration = new Configuration({ apiKey });
     return new OpenAIApi(configuration);
   }
 
-  async callChatGpt() {
+  async callChatGpt(): Promise<void> {
     this.isLoading = true;
 
-    let inputText = this.message;
+    const model = this.versionGPT === '4' ? 'gpt-4' : 'gpt-3.5-turbo';
+    const inputText = this.message;
 
     const response = await this.openAi.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      //   model: 'gpt-4',
+      model,
       messages: [{ role: 'system', content: inputText }],
     });
 
-    const resultChatGPT = response.data.choices[0].message.content;
+    const chatResponse = response.data.choices[0].message.content;
     this.getTokensUsedCost(response.data.usage.total_tokens);
 
-    if (this.version === '1') await this.version1(inputText, resultChatGPT);
-    if (this.version === '2') await this.version2(inputText, resultChatGPT);
-    if (this.version === '3') await this.version3(inputText, resultChatGPT);
-    if (this.version === '4') await this.version4(inputText, resultChatGPT);
-    if (this.version === '5') await this.version5(inputText, resultChatGPT);
+    await this.callBot(inputText, chatResponse);
 
     this.isLoading = false;
   }
 
-  getTokensUsedCost(tokensUsed) {
-    const COST_PER_TOKEN = 0.000002;
-
+  getTokensUsedCost(tokensUsed: number): void {
+    const COST_PER_TOKEN = this.versionGPT === '4' ? 0.00008 : 0.000002;
     const cost = tokensUsed * COST_PER_TOKEN;
-
     this.costMsg = `Tokens used: ${tokensUsed} \n Cost: ${cost.toFixed(4)}`;
   }
-
-  version1(inputText: string, resultChatGPT: string) {
-    console.log(resultChatGPT);
-  }
-
-  version2(inputText: string, resultChatGPT: string) {
-    console.log(resultChatGPT);
-
-    this.chatHistory.push({ message: inputText, response: resultChatGPT });
-    this.message = '';
-  }
-
-  version3(inputText: string, resultChatGPT: string) {
-    console.log(resultChatGPT);
-
+  async callBot(inputText: string, chatResponse: string): Promise<void> {
+    console.log(chatResponse);
     const userMessage = { message: inputText, response: '', type: 'user' };
-    const botMessage = { message: resultChatGPT, response: '', type: 'bot' };
+    const botMessage = { message: chatResponse, response: '', type: 'bot' };
 
     this.chatHistory.push(userMessage);
     this.chatHistory.push(botMessage);
 
     this.message = '';
+    // await this.speakText(chatResponse);
   }
 
-  version4(inputText: string, resultChatGPT: string) {
-    console.log(resultChatGPT);
-
-    const userMessage = { message: inputText, response: '', type: 'user' };
-    const botMessage = { message: resultChatGPT, response: '', type: 'bot' };
-
-    this.chatHistory.push(userMessage);
-    this.chatHistory.push(botMessage);
-
-    this.message = '';
-  }
-
-  version5(inputText: string, resultChatGPT: string) {
-    console.log(resultChatGPT);
-
-    const userMessage = { message: inputText, response: '', type: 'user' };
-    const botMessage = { message: resultChatGPT, response: '', type: 'bot' };
-
-    this.chatHistory.push(userMessage);
-    this.chatHistory.push(botMessage);
-
-    this.message = '';
-    this.speakText(resultChatGPT);
-  }
-
-  speakText(text: string) {
+  async speakText(text: string): Promise<void> {
     const synth = window.speechSynthesis;
-
-    // Check if speech synthesis is supported by the browser
     if (synth) {
-      console.log('🚀 ~ ChatgptComponent ~ speakText ~ synth:', synth);
       const utterance = new SpeechSynthesisUtterance(text);
       synth.speak(utterance);
     } else {
